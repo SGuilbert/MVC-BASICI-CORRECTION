c8bf89a5f061af1f131b559fedcebd70d02d9322
sguilbertPPR <sylvain.guilbert@lyceeppr.fr>
2020-05-12 16:02:54 +0200

    first

--- libs/Application.php
+++ libs/Application.php
@@ -10,9 +10,13 @@
 
 class Application
 {
+    private $controller = null;
+    private $action = null;
+    private $params = null;
+
     public function __construct()
     {
-        $controllerDirectory = $_SERVER['DOCUMENT_ROOT'] . '/controllers/'; // emplacement par défaut de nos controllers
+        /*$controllerDirectory = $_SERVER['DOCUMENT_ROOT'] . '/controllers/'; // emplacement par défaut de nos controllers
         // 1. router
         $tokens = explode('/', rtrim($_SERVER['REQUEST_URI'], '/'));
 
@@ -40,6 +44,75 @@ class Application
             $controllerName = 'ErrorController';
             $controller = new $controllerName;
             $controller->IndexAction();
+        }*/
+        $this->parseURL();
+        $this->route();
+        $this->dispatch();
+
+    }
+
+    /**
+     * Decoupe l'url
+     * initialise les attributs controller, action, params
+     */
+    private function parseURL(){
+        $tokens = explode('/', rtrim($_SERVER['REQUEST_URI'], '/'));
+        if(isset($tokens[1])){//le parametre controller existe dans l'url
+            $this->controller = $tokens[1];
+            if (isset($tokens[2])){//le parametre action existe dans l'url
+                $this->action=$tokens[2];
+                if (isset($tokens[3])){
+                    $params = $tokens[3];
+                }
+            }
+            else{//pas de parametre action => index
+                $this->action = 'index';
+            }
         }
+        else{//pas de parametre controller=> index
+            $this->controller = 'index';
+        }
+    }
+
+    /**+
+     * instancie le controller
+     *  -reconstruit le nom
+     *  -vérifie l'existence de la classe
+     *  -instancie le controller adhoc dans l'attribut controller
+     */
+    private function route(){
+        $controllerDirectory = $_SERVER['DOCUMENT_ROOT'] . '/controllers/'; // emplacement par défaut de nos controllers
+        $this->controller .= 'Controller'; // on complete pour coller à la convention de nommage
+        if (file_exists($controllerDirectory. $this->controller . '.php')){
+            require_once($controllerDirectory . $this->controller . '.php'); // inclusion de la classe (meme nom de fichier que la classe)
+            $this->controller = new $this->controller;
+        }
+        else{
+            $this->error('Controller not found');
+        }
+    }
+
+    /**
+     * -reconstruit le nom
+     * -verifie l'existance de la méthode correspondant à l'action
+     * -execute la méthode et lui passe les paramètres
+     */
+    private function dispatch(){
+        $this->action = $this->action . 'Action';// on complete pour coller à la convention de nommage
+        if (method_exists($this->controller,$this->action)){
+            if($this->params){
+                $this->controller->{$this->action}($this->params);
+            }
+            else{
+                $this->controller->{$this->action}();
+            }
+        }
+        else{
+            $this->error('actionMethod does not exists');
+        }
+    }
+
+    private function error($message){
+        echo "<h1>{$message}</h1>";
     }
 }
